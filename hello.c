8b991457dc1da662455f868945bb9ea5366ab2bd
Abduvokhidjon <aabduvohid33@gmail.com>
2023-06-03 12:11:56 -0500

    cs50_week_1

--- hello.c
+++ hello.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include "cs50.h"
 
-int main(void)
+int main(void) {
+// {
+//     string first = get_string("What's your first name ? ");
+//     string last = get_string("What's your last name? ");
+//     printf("hello, %s %s\n", first, last);
+// }
+int x = 5;
+int y = 7;
+
+if (x < y) 
 {
-    string first = get_string("What's your first name ? ");
-    string last = get_string("What's your last name? ");
-    printf("hello, %s %s\n", first, last);
+ printf("x is less than y\n");
+}
+else if (x > y)
+{
+ printf("x is greater than y\n");
+}
+else
+{
+ printf("x is equal to y\n");
+}
 }
